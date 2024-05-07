cd41b040580979d5bff0858f8ad4828472b57ede
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-05-07 14:53:33 +0800

    update store request alternatif

--- app/Http/Requests/StoreAlternatifRequest.php
+++ app/Http/Requests/StoreAlternatifRequest.php
@@ -11,7 +11,7 @@ class StoreAlternatifRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class StoreAlternatifRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'nama_alternatif'=> 'required|string|max:50',
+            'penilaian'=> 'required|array',
+            'penilaian.*.kriteria'=> 'required|exists:kriteria,id',
+            'penilaian.*.nilai'=> 'required|numeric',
         ];
     }
 }
