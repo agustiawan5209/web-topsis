6a6206b2e551b7cec872c1296ca113765ce45178
Agustiawan <44895587+thype5209@users.noreply.github.com>
2024-05-07 10:54:50 +0800

    Buat Model || Controller Dari Kriteria

---
+++ app/Http/Controllers/KriteriaController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use App\Models\Kriteria;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Request;
+use App\Http\Requests\StoreKriteriaRequest;
+use App\Http\Requests\UpdateKriteriaRequest;
+
+class KriteriaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+        $tableName = 'kriterias'; // Ganti dengan nama tabel yang Anda inginkan
+        $columns = DB::getSchemaBuilder()->getColumnListing($tableName);
+
+        //
+        return Inertia::render('Admin/Kriteria/Index', [
+            'search' =>  Request::input('search'),
+            'table_colums'=> array_values(array_diff($columns, ['remember_token','password', 'email_verified_at', 'created_at', 'updated_at'])),
+            'data'=> Kriteria::filter(Request::only('search','order'))->role('Pengguna')->paginate(10),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return Inertia::render('Admin/Kriteria/Create', []);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKriteriaRequest $request)
+    {
+        $data = $request->all();
+
+        $arr = collect($request->nama);
+        $bobot = collect($request->bobot);
+        for ($i=0; $i < count($arr); $i++) {
+            if(isset($arr[$i]) && isset($bobot[$i])){
+                Kriteria::created([
+                    'nama'=> $arr[$i],
+                    'bobot'=> $bobot[$i],
+                ]);
+            }
+        }
+
+        return redirect()->route('Kriteria.index')->with('message', 'Data Kriteria Berhasil Di Tambah!!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Kriteria $kriteria)
+    {
+        return Inertia::render('Admin/Kriteria/Show',[
+            'kriteria'=> $kriteria->find(Request::input('slug')),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Kriteria $kriteria)
+    {
+        return Inertia::render('Admin/Kriteria/Edit',[
+            'kriteria'=> $kriteria->find(Request::input('slug')),
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKriteriaRequest $request, Kriteria $kriteria)
+    {
+        $kriteria = $kriteria->find(Request::input('slug'));
+        $kriteria->update($request->all());
+        return redirect()->route('Kriteria.index')->with('message', 'Data Kriteria Berhasil Di Edit!!');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Kriteria $kriteria)
+    {
+        $kriteria->find(Request::input('slug'))->delete();
+        return redirect()->route('Kriteria.index')->with('message', 'Data Kriteria Berhasil Di Hapus!!');
+
+    }
+}
---
+++ app/Models/Kriteria.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kriteria extends Model
+{
+    use HasFactory;
+
+    protected $table = 'kriterias';
+    protected $fillable = [
+        'nama',
+        'bobot',
+    ];
+
+
+     //  FIlter Data User
+     public function scopeFilter($query, $filter)
+     {
+         $query->when($filter['search'] ?? null, function ($query,$search)  {
+             $query->where('nama', 'like', '%' . $search . '%');
+         })->when($filter['order'] ?? null, function ($query,$order)  {
+             $query->orderBy('id', $order);
+         });
+     }
+}
